c557a2b1b622b6a1c38d2329f687c18e92a68912
Ndiaye <elhadjiamadoundiaye@gmail.com>
2021-02-07 11:12:54 +0000

    Create Employee model & repository

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,13 +1,32 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//let's use jpa annotation to map this model to the listener database table
+@Entity
+@Table(name= "employees")
 public class Employee {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "first_name")
 	private String firstname;
+	
+	@Column(name= "last_name")
 	private String lastname;
+	
+	@Column(name= "email")
 	private String email;
 	
-	//default constructor
+	
+	// default constructor
 	public Employee () {
 		
 	}
